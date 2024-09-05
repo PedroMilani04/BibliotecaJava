6b6af0e2c440baa5581f16c7d4ece7cb2b4f78ca
Pedro Henrique Milani Vagula <89487697+PedroMilani04@users.noreply.github.com>
2024-09-05 15:18:26 -0300

    Create GerenciarEmprestimo.java

---
+++ GerenciarEmprestimo.java
@@ -0,0 +1,29 @@
+public class GerenciarEmprestimo {
+
+    private static GerenciarEmprestimo instancia;
+
+    // Construtor privado para evitar instância externa
+    private GerenciarEmprestimo() {
+    }
+
+    // Método para obter a instância única
+    public static synchronized GerenciarEmprestimo getInstancia() {
+        if (instancia == null) {
+            instancia = new GerenciarEmprestimo();
+        }
+        return instancia;
+    }
+
+    public EmprestimoLivro realizarEmprestimo(USUARIO user, LIVRO livro) {
+        if (user.possuiMulta) {
+            return null;
+        } else {
+            if (Catalogo.disponivel(livro)) {
+                LocalDate dataComeco = LocalDate.now();
+                LocalDate dataFim = dataComeco.plus(Period.ofDays(30));
+                return new EmprestimoLivro(livro, user, dataComeco, dataFim);
+            }
+        }
+        return null;
+    }
+}
