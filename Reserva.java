40270cc27582fd56c7b5b64291165503dd2d5df4
cristianeidi <eidiyoshi@hotmail.com>
2024-09-05 14:07:02 -0300

    fix

--- Reserva.java
+++ Reserva.java
@@ -1,8 +1,12 @@
+package com.mycompany.mavenproject2;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
 
+import POOII.BibliotecaJava.UsuarioBiblioteca;
+import java.time.LocalDate;
 import java.util.ArrayList;
 /**
  *
@@ -11,27 +15,77 @@ import java.util.ArrayList;
 public class Reserva {
 
     private Livro livroReservado;
-    private ArrayList<UsuarioBiblioteca>;
+    private ArrayList<UsuarioBiblioteca> arrayUsuarios;
+    private ArrayList<LocalDate> arrayTempo; // para manter track dos usuarios e seus respectivos tempos
     
-    public boolean reservarLivro(Livro livro){ // adicionado = true, ja ter na reserva = false
-        for( int contador = 0; contador < numeroLivroReservados; contador ++){
-            if( livro.equals(livroReservados.get(contador)) ){
-                return false;
-            }
-        }
-        livroReservados.add(livro);
-        return true;
+    public Reserva( Livro livro, UsuarioBiblioteca usuario ) {
+        this.livroReservado = livro;
+        
+        arrayUsuarios = new ArrayList<UsuarioBiblioteca>();
+        arrayUsuarios.add( usuario );
+        
+        arrayTempo = new ArrayList<LocalDate>();
+        arrayTempo.add( LocalDate.now() );
+        
+    }
+
+    public void adicionarUsuarioReserva( UsuarioBiblioteca usuario ){
+        arrayUsuarios.add(usuario);
+        arrayTempo.add( LocalDate.now() );
+        
     }
     
-    public boolean cancelarReservarLivro(Livro livro){ // cancelado = true, nao encontrou = false
-        for( int contador = 0; contador < numeroLivroReservados; contador ++){
-            if( livro.equals(livroReservados.get(contador)) ){
-                livroReservados.remove(contador);
-                return true;
+    
+    public UsuarioBiblioteca retirarProximoUsuario(){
+        UsuarioBiblioteca temporario = arrayUsuarios.get(0);
+        arrayUsuarios.remove(0);
+        arrayTempo.remove(0);
+        return temporario;
+    }
+    
+    public UsuarioBiblioteca retirarEspecificoUsuario( UsuarioBiblioteca usuario ){
+        for( int contador = 0, numeroDeUsuarios = arrayUsuarios.size(); contador < numeroDeUsuarios ; contador++ ){
+            if( usuario.equals(arrayUsuarios.get(contador)) ){
+                UsuarioBiblioteca temporario = arrayUsuarios.get(contador);
+                arrayUsuarios.remove(contador);
+                arrayTempo.remove(contador);
+                return temporario;
             }
         }
-        return false;
+        // caso nao encontre o usuario, returna o proximo
+        return retirarProximoUsuario();
     }
     
     
+    public UsuarioBiblioteca checarProximoUsuario(){
+        return arrayUsuarios.get(0);
+    }
+    
+    public LocalDate checarProximoTempo(){
+        return arrayTempo.get(0);
+    }
+    
+    
+    public UsuarioBiblioteca checarUltimoUsuario(){
+        return arrayUsuarios.get(arrayUsuarios.size());
+    }
+    
+    public LocalDate checarUltimoTempo(){
+        return arrayTempo.get(arrayTempo.size());
+    }
+    
+    public void exibirTodosUsuarios(){
+        int contadorTamanho = arrayUsuarios.size(), ano,mes,dia ;
+        
+        for( int contador = 0; contador < contadorTamanho; contador++ ){
+            
+            String nome = arrayUsuarios.get(contador).getNome();
+            System.out.println("Nome do Usuario: " + nome);
+            
+            ano = arrayTempo.get(contador).getYear();
+            mes = arrayTempo.get(contador).getMonthValue();
+            dia = arrayTempo.get(contador).getDayOfMonth();
+            System.out.println("Data: " + dia + "/" + mes + "/" + ano);
+        }
+    }
 }
