ab91c1254d0893dbf027267c922093c6f8c644e9
sleeper <nicolas.s.fernades@unesp.br>
2024-10-30 17:26:22 -0300

    Melhora na captação de dados na hora do cadastro

--- Biblioteca/Cadastro.java
+++ Biblioteca/Cadastro.java
@@ -11,7 +11,14 @@ public class Cadastro {
     protected Hashtable<String, Cadastro> ht_email_for_cad = new Hashtable<String, Cadastro>(); //Criando uma hashtable para armazenar os emails e os cadastros (acha o cadas usando o email)
     protected Hashtable<String, Cadastro> ht_nickname_for_cad = new Hashtable<String, Cadastro>(); //Criando uma hashtable para armazenar os nicknames e os cadastros (acha o cadas usando o nickname)
 
-    //Procurar como fazer uma hashtable pra por nicknames dos emails (maior nota de criatividade, ou entra com o email ou entra com o nickname
+    //Procurar como fazer uma hashtable pra por nicknames dos emails (maior nota de criatividade, ou entra com o email ou entra com o nickname //////////FEITO!!!!
+
+    //criptografar talvez? (não é necessário, mas é uma boa prática)
+
+    //Criar verificação de CPF tanto aqui como na classe usuario, talvez fazer com que n bata já na função a senha incompativel com a confirmação e menos de 8 (FEITO!!)
+    //Adicionar isso dps (boa gpt lindo perfeito!!!!) 9FEITO !!
+
+    //00:04 e ninguém imagina como tá a mente do palhaço
 
     //criar um método que iria mostrar o que no hashtable porém ele será privado, só pra uso interno
 
@@ -28,7 +35,7 @@ public class Cadastro {
         return email;
     }
 
-    public void criarConta(){
+    public void criarConta(){ //Criação de conta e verificação de dados para não bater em outro na hora de criar
         String nome;
         String CPF;
         String nickname;
@@ -36,16 +43,49 @@ public class Cadastro {
         Scanner input = new Scanner(System.in);
         System.out.println("Digite seu email: ");
         email = input.nextLine();
+
+        while(ht_email_for_cad.containsKey(email)){
+            System.out.println("Email já cadastrado. Por favor, escolha outro.");
+            email = input.nextLine();
+        }
+
         System.out.println("Digite sua senha: ");
         senha = input.nextLine();
+
+        while(senha.length() <= 8){
+            System.out.println("Senha muito curta, tente novamente");
+            senha = input.nextLine();
+        }
+
         System.out.println("Confirme sua senha: ");
         confirmacao = input.nextLine();
+
+        while(!senha.equals(confirmacao)){
+            System.out.println("Senhas não conferem, tente novamente");
+            System.out.println("Digite sua senha: ");
+            senha = input.nextLine();
+            System.out.println("Confirme sua senha: ");
+            confirmacao = input.nextLine();
+        }
+
         System.out.println("Digite seu nome: ");
         nome = input.nextLine();
         System.out.println("Digite seu CPF: ");
         CPF = input.nextLine();
+
+        while(CPF.length() != 11){
+            System.out.println("CPF inválido, tente novamente");
+            CPF = input.nextLine();
+        }
+
         System.out.println("Digite um nickname: "); //procurar no banco de dados se já existe criar dps[...]
         nickname = input.nextLine();
+
+        while(ht_nickname_for_cad.containsKey(nickname)) {
+            System.out.println("Nickname já cadastrado. Por favor, escolha outro.");
+            nickname = input.nextLine();
+        }
+
         Cadastro cadastro = new Cadastro(email, senha, confirmacao);
         Usuario usuario = new Usuario(nome, email, CPF, nickname);
 
@@ -66,7 +106,7 @@ public class Cadastro {
             return retorna;
         }else{
             System.out.println("Usuário não encontrado");
-            return null;
+            return null; //configurar para dar erro ao retornar null
         }
     }
 
